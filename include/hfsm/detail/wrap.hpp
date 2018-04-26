43ab48234d706371a445a3bc971276d2f9381a5a
Andrew Gresyk <andrew-gresyk@users.noreply.github.com>
2018-04-26 19:30:31 +0100

    * fixed windows 32bit build

--- include/hfsm/detail/wrap.hpp
+++ include/hfsm/detail/wrap.hpp
@@ -37,16 +37,13 @@ public:
 	inline		 T* operator->()							{ return &get();				}
 	inline const T* operator->() const						{ return &get();				}
 
-	inline explicit operator bool() const					{ return integer() != 0;		}
+	inline explicit operator bool() const					{ return _storage != 0;			}
 
 	inline bool operator == (const Wrap other) const		{ return get() == other.get();	}
 
 private:
 	inline		 T& get()			{ return *reinterpret_cast<		 T* const>(&_storage);	}
 	inline const T& get() const		{ return *reinterpret_cast<const T* const>(&_storage);	}
-
-	inline unsigned integer() const { return *reinterpret_cast<const unsigned* const>(&_storage); }
-	static_assert(sizeof(unsigned) < sizeof(Storage), "");
 
 private:
 	Storage _storage;
