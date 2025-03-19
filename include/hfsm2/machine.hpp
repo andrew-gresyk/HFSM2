b79de8833793f06879f9e14ee267c216261c58c9
Andrew Gresyk <andrew-gresyk@users.noreply.github.com>
2025-03-19 12:32:36 +0200

    2.6.1
    Bumping the version for vcpkg release

--- include/hfsm2/machine.hpp
+++ include/hfsm2/machine.hpp
@@ -1,5 +1,5 @@
 ﻿// HFSM2 (hierarchical state machine for games and interactive applications)
-// 2.6.0 (2025-02-01)
+// 2.6.1 (2025-03-19)
 //
 // Created by Andrew Gresyk
 //
@@ -33,7 +33,7 @@
 
 #define HFSM2_VERSION_MAJOR 2
 #define HFSM2_VERSION_MINOR 6
-#define HFSM2_VERSION_PATCH 0
+#define HFSM2_VERSION_PATCH 1
 
 #define HFSM2_VERSION (10000 * HFSM2_VERSION_MAJOR + 100 * HFSM2_VERSION_MINOR + HFSM2_VERSION_PATCH)
 
