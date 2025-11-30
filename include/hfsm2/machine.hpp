b63b5d082df7bacb54d515221c930f86f2e362af
Andrew Gresyk <andrew-gresyk@users.noreply.github.com>
2025-11-30 17:18:33 +0200

    removing g++-9

--- include/hfsm2/machine.hpp
+++ include/hfsm2/machine.hpp
@@ -1,5 +1,5 @@
 ﻿// HFSM2 (hierarchical state machine for games and interactive applications)
-// 2.9.0 (2025-11-07)
+// 2.10.0 (2025-11-30)
 //
 // Created by Andrew Gresyk
 //
@@ -32,7 +32,7 @@
 #pragma once
 
 #define HFSM2_VERSION_MAJOR 2
-#define HFSM2_VERSION_MINOR 9
+#define HFSM2_VERSION_MINOR 10
 #define HFSM2_VERSION_PATCH 0
 
 #define HFSM2_VERSION (10000 * HFSM2_VERSION_MAJOR + 100 * HFSM2_VERSION_MINOR + HFSM2_VERSION_PATCH)
